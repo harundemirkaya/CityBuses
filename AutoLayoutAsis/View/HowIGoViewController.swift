b74d0f144deba859516fcdfd3325305348816c06
harundemirkaya <harundmrk@gmail.com>
2023-02-20 11:51:15 +0300

    Added Stations TextField and TableView

--- AutoLayoutAsis/View/HowIGoViewController.swift
+++ AutoLayoutAsis/View/HowIGoViewController.swift
@@ -11,7 +11,7 @@ import CoreLocation
 import MapKit
 
 // MARK: -HowIGoViewController Class
-class HowIGoViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+class HowIGoViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource {
 
     // MARK: -Define
     
@@ -73,15 +73,151 @@ class HowIGoViewController: UIViewController, CLLocationManagerDelegate, MKMapVi
         return btn
     }()
     
+    // MARK: TextField Defined
+    var txtFieldFrom: UITextField = {
+        let txtField = UITextField()
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        txtField.placeholder = "from".localized()
+        txtField.layer.cornerRadius = 4.0
+        txtField.layer.borderWidth = 1.0
+        txtField.layer.borderColor = UIColor.gray.cgColor
+        let paddingViewUsername = UIView(frame: CGRectMake(0, 0, 15, txtField.frame.height))
+        txtField.leftView = paddingViewUsername
+        txtField.leftViewMode = UITextField.ViewMode.always
+        txtField.autocorrectionType = .no
+        txtField.autocapitalizationType = .none
+        return txtField
+    }()
+        
+    var txtFieldTo: UITextField = {
+        let txtField = UITextField()
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        txtField.placeholder = "to".localized()
+        txtField.layer.cornerRadius = 4.0
+        txtField.layer.borderWidth = 1.0
+        txtField.layer.borderColor = UIColor.gray.cgColor
+        let paddingViewUsername = UIView(frame: CGRectMake(0, 0, 15, txtField.frame.height))
+        txtField.leftView = paddingViewUsername
+        txtField.leftViewMode = UITextField.ViewMode.always
+        txtField.autocorrectionType = .no
+        txtField.autocapitalizationType = .none
+        return txtField
+    }()
+    
+    // MARK: StationsView Defined
+    var stationsView: UIView = {
+        var view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    // MARK: TableViews Defined
+    private lazy var tableViewFrom : UITableView = {
+        let table = UITableView()
+        table.delegate = self
+        table.dataSource = self
+        table.backgroundColor = .white
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(MenuTableViewCell.self, forCellReuseIdentifier: MenuTableViewCell.identifier)
+        return table
+    }()
+        
+    private lazy var tableViewTo : UITableView = {
+        let table = UITableView()
+        table.delegate = self
+        table.dataSource = self
+        table.backgroundColor = .white
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(MenuTableViewCell.self, forCellReuseIdentifier: MenuTableViewCell.identifier)
+        return table
+    }()
+    
+    // MARK: Stations Data
+    var stations: [Stop]? {
+        didSet{
+            if stations!.count != 0 {
+                for i in 0...stations!.count-1{
+                    stationsName.append(stations![i].name!)
+                }
+            }
+            filteredStations = stationsName
+            tableViewFrom.reloadData()
+            tableViewTo.reloadData()
+        }
+    }
+    var stationsName: [String] = []
+    var filteredStations: [String] = []
+    
+    // MARK: StackView
+    var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    // MARK: Close Keyboard Button
+    var btnCloseKeyboard: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Close Keyboard", for: UIControl.State.normal)
+        btn.setTitleColor(.black, for: UIControl.State.normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .white
+        return btn
+    }()
+    
+    var fromID = 0
+    var toID = 0
+    
+    // MARK: From and To Location
+    var fromLocation: CLLocation = CLLocation(latitude: 0, longitude: 0){
+        didSet{
+            if !stations!.isEmpty{
+                for i in 0...stations!.count-1{
+                    if stations![i].latitude == fromLocation.coordinate.latitude, stations![i].longitude == fromLocation.coordinate.longitude{
+                        fromID = stations![i].stopID!
+                    }
+                }
+            }
+        }
+    }
+    var toLocation: CLLocation = CLLocation(latitude: 0, longitude: 0){
+        didSet{
+            if !stations!.isEmpty{
+                for i in 0...stations!.count-1{
+                    if stations![i].latitude == toLocation.coordinate.latitude, stations![i].longitude == toLocation.coordinate.longitude{
+                        toID = stations![i].stopID!
+                    }
+                }
+            }
+        }
+    }
+    
+    let howIGoViewModel = HowIGoViewModel()
+    
     // MARK: -ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // MARK: -Screen
+        // MARK: Screen
         view.backgroundColor = .white
         
+        // MARK: Get Stations
+        howIGoViewModel.howIGoVC = self
+        howIGoViewModel.getStations()
+        
+        // MARK: Set Station View Constraints
+        stationsView.stationsViewConstraints(view)
+        txtFieldFrom.txtFieldFromConstraints(stationsView)
+        txtFieldTo.txtFieldToConstraints(stationsView, txtFieldFrom: txtFieldFrom)
+        stackView.stackViewConstraints(view, stationsView: stationsView)
+        
         // MARK: Map Config
-        mapView.mapConstraints(view)
+        mapView.setMapConstraints(stackView)
         self.locationManager.delegate = self
         self.locationManager.requestAlwaysAuthorization()
         self.locationManager.requestWhenInUseAuthorization()
@@ -94,6 +230,22 @@ class HowIGoViewController: UIViewController, CLLocationManagerDelegate, MKMapVi
         }
         mapView.delegate = self
         mapView.showsUserLocation = true
+        
+        // MARK: TextFields
+        tableViewFrom.tableViewConstraints(stackView)
+        tableViewTo.tableViewConstraints(stackView)
+        tableViewFrom.transform = CGAffineTransform(translationX: 0, y: view.frame.height)
+        tableViewTo.transform = CGAffineTransform(translationX: 0, y: view.frame.height)
+        txtFieldFrom.addTarget(self, action: #selector(txtFieldStartTableFrom), for: .editingDidBegin)
+        txtFieldTo.addTarget(self, action: #selector(txtFieldStartTableTo), for: .editingDidBegin)
+        
+        txtFieldFrom.inputAccessoryView = btnCloseKeyboard
+        txtFieldTo.inputAccessoryView = btnCloseKeyboard
+        btnCloseKeyboard.addTarget(self, action: #selector(closeKeyboard), for: .touchUpInside)
+            
+        // MARK: Search Algorithm
+        txtFieldFrom.addTarget(self, action: #selector(textFieldFromDidChange), for: .editingChanged)
+        txtFieldTo.addTarget(self, action: #selector(textFieldToDidChange), for: .editingChanged)
         
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
         mapView.addGestureRecognizer(longPressRecognizer)
@@ -198,6 +350,91 @@ class HowIGoViewController: UIViewController, CLLocationManagerDelegate, MKMapVi
         routeVC.routes = routeMaps?.routes
         navigationController?.pushViewController(routeVC, animated: true)
     }
+    
+    // MARK: -TextFields Did Change Functions
+    @objc func textFieldFromDidChange(){
+        filteredStations = []
+        if txtFieldFrom.text == ""{
+            filteredStations = stationsName
+        } else{
+            for station in stationsName{
+                if station.lowercased().contains(txtFieldFrom.text!.lowercased()){
+                    filteredStations.append(station)
+                }
+            }
+        }
+        self.tableViewFrom.reloadData()
+    }
+    
+    @objc func textFieldToDidChange(){
+        filteredStations = []
+        if txtFieldTo.text == ""{
+            filteredStations = stationsName
+        } else{
+            for station in stationsName{
+                if station.lowercased().contains(txtFieldTo.text!.lowercased()){
+                    filteredStations.append(station)
+                }
+            }
+        }
+        self.tableViewTo.reloadData()
+    }
+    
+    // MARK: -CloseKeyboard Function
+    @objc func closeKeyboard(){
+        txtFieldStopTable()
+    }
+    
+    // MARK: -Open TableViews
+    @objc func txtFieldStartTableFrom() {
+        UIView.animate(withDuration: 0.3) {
+            self.tableViewFrom.transform = CGAffineTransform(translationX: 0, y: 0)
+        }
+    }
+    @objc func txtFieldStartTableTo() {
+        UIView.animate(withDuration: 0.3) {
+            self.tableViewTo.transform = CGAffineTransform(translationX: 0, y: 0)
+        }
+    }
+    
+    // MARK: -Close TableView and Keyboard
+    @objc func txtFieldStopTable() {
+        txtFieldFrom.endEditing(true)
+        txtFieldTo.endEditing(true)
+        UIView.animate(withDuration: 0.3) {
+            self.tableViewFrom.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height)
+            self.tableViewTo.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height)
+        }
+    }
+    
+    // MARK: -TableView Config
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == tableViewFrom{
+            return filteredStations.count
+        } else{
+            return filteredStations.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == tableViewFrom{
+            let cell = tableView.dequeueReusableCell(withIdentifier: MenuTableViewCell.identifier, for: indexPath)
+            cell.textLabel?.text = String(indexPath.row + 1) + "- " + (filteredStations[indexPath.row])
+        return cell
+            } else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: MenuTableViewCell.identifier, for: indexPath)
+            cell.textLabel?.text = String(indexPath.row + 1) + "- " + (filteredStations[indexPath.row])
+            return cell
+        }
+    }
+    
+    // MARK: -Show Alert Message
+    func alertMessage(title: String, description: String){
+        let alertMessage = UIAlertController(title: title, message: description, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: NSLocalizedString("btnOkey", comment: "Alert Okey Button"), style: UIAlertAction.Style.default)
+        alertMessage.addAction(okButton)
+        self.present(alertMessage, animated: true)
+    }
 }
 
 public extension UIView{
@@ -220,5 +457,55 @@ public extension UIView{
         view.addSubview(self)
         topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
         trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+    }
+    
+    func stationsViewConstraints(_ view: UIView){
+        view.addSubview(self)
+        topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        heightAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+    
+    func txtFieldFromConstraints(_ view: UIView){
+        view.addSubview(self)
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85).isActive = true
+        heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+        
+    func txtFieldToConstraints(_ view: UIView, txtFieldFrom: UITextField){
+        view.addSubview(self)
+        topAnchor.constraint(equalTo: txtFieldFrom.bottomAnchor, constant: 10).isActive = true
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85).isActive = true
+        heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+        
+    func tableViewConstraints(_ view: UIStackView){
+        view.addSubview(self)
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+        
+    func stackViewConstraints(_ view: UIView, stationsView: UIView){
+        view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: stationsView.bottomAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
+    func setMapConstraints(_ view: UIStackView){
+        view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
     }
 }
--- AutoLayoutAsis/ViewModel/HowIGoViewModel.swift
+++ AutoLayoutAsis/ViewModel/HowIGoViewModel.swift
@@ -10,4 +10,17 @@ import Foundation
 // MARK: -HowIGo View Model
 class HowIGoViewModel{
     var howIGoVC: HowIGoViewController?
+    
+    // MARK: Network Manager Defined
+    var networkManager = NetworkManager()
+    
+    func getStations(){
+        networkManager.fetchStations { [weak self] result in
+            if result.response?.statusCode == 200{
+                self?.howIGoVC?.stations = result.value?.stops
+            } else{
+                self?.howIGoVC?.alertMessage(title: NSLocalizedString("errorTitle", comment: "Error"), description: result.error!.localizedDescription)
+            }
+        }
+    }
 }
