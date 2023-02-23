5c6ab2d5e180b1d17ed8c3a3aa59376ef221ace9
harundemirkaya <harundmrk@gmail.com>
2023-02-23 10:31:42 +0300

    Added SSL Certificate

--- AutoLayoutAsis/Services/NetworkManager.swift
+++ AutoLayoutAsis/Services/NetworkManager.swift
@@ -23,44 +23,90 @@ class NetworkManager {
     
     public var longitude: Double?
     
+    static let sharedInstance = NetworkManager()
+    let session: Session
+    let serverTrustPolicies: [String: ServerTrustEvaluating]
+        
+    public init() {
+        serverTrustPolicies = [
+            "tfe-opendata.com": PinnedCertificatesTrustEvaluator(certificates: [NetworkManager.certificate()], acceptSelfSignedCertificates: false)
+        ]
+        
+        self.session = Session(serverTrustManager: ServerTrustManager(evaluators: serverTrustPolicies))
+            
+        let sessionConfiguration = URLSessionConfiguration.af.default
+        sessionConfiguration.timeoutIntervalForResource = 60
+        sessionConfiguration.timeoutIntervalForRequest = 60
+        sessionConfiguration.httpAdditionalHeaders = HTTPHeaders.default.dictionary
+        sessionConfiguration.requestCachePolicy = .useProtocolCachePolicy
+        sessionConfiguration.urlCredentialStorage = nil
+    }
+        
+    static func certificate() -> SecCertificate {
+        // Burada kullanacağınız sertifikanın dosya yolunu belirleyin.
+        // Örneğin, bundle içinde bir sertifika dosyası varsa, aşağıdaki gibi kullanılabilir:
+        guard let certificatePath = Bundle.main.path(forResource: "open-data", ofType: "cer") else {
+            fatalError("Sertifika bulunamadı.")
+        }
+            
+        let certificateData = try! Data(contentsOf: URL(fileURLWithPath: certificatePath))
+        guard let certificate = SecCertificateCreateWithData(nil, certificateData as CFData) else {
+            fatalError("Sertifika oluşturulamadı.")
+        }
+        return certificate
+    }
+    
+    
     // MARK: -Fetch Stations
-    public func fetchStations(completion: @escaping (_ result: DataResponse<StationModel, AFError>) -> Void) {
-        AF.request("https://tfe-opendata.com/api/v1/stops").responseDecodable(of: StationModel.self) { response in
+    public func fetchStations(completion: @escaping (DataResponse<StationModel, AFError>) -> Void) {
+        session.request("https://tfe-opendata.com/api/v1/stops")
+            .validate()
+            .responseDecodable(of: StationModel.self) { response in
             completion(response)
         }
     }
     
     // MARK: -Fetch Services
     public func fetchServices(completion: @escaping (_ result: DataResponse<ServiceModel, AFError>) -> Void) {
-        AF.request("https://tfe-opendata.com/api/v1/services").responseDecodable(of: ServiceModel.self) { response in
+        session.request("https://tfe-opendata.com/api/v1/services")
+            .validate()
+            .responseDecodable(of: ServiceModel.self) { response in
             completion(response)
         }
     }
     
     // MARK: -Fetch Live Vehicles
     public func fetchLiveVehicles(completion: @escaping (_ result: DataResponse<VehicleModel, AFError>) -> Void) {
-        AF.request("https://tfe-opendata.com/api/v1/vehicle_locations").responseDecodable(of: VehicleModel.self) { response in
+        session.request("https://tfe-opendata.com/api/v1/vehicle_locations")
+            .validate()
+            .responseDecodable(of: VehicleModel.self) { response in
             completion(response)
         }
     }
     
     // MARK: -Fetch Stop Services
     public func fetchStopServices(completion: @escaping (_ result: DataResponse<StopServices, AFError>) -> Void) {
-        AF.request("https://tfe-opendata.com/api/v1/timetables/\(stopID!)").responseDecodable(of: StopServices.self) { response in
+        session.request("https://tfe-opendata.com/api/v1/timetables/\(stopID!)")
+            .validate()
+            .responseDecodable(of: StopServices.self) { response in
             completion(response)
         }
     }
     
     // MARK: -Fetch RouteMaps
     public func fetchRouteMaps(completion: @escaping (_ result: DataResponse<RouteMaps, AFError>) -> Void) {
-        AF.request("https://maps.googleapis.com/maps/api/directions/json?alternatives=true&key=\(apiKey!)&destination=\(destination!)&origin=\(origin!)&mode=transit&transit_routing_preference=fewer_transfers&transit_mode=bus&language=tr").responseDecodable(of: RouteMaps.self) { response in
+        session.request("https://maps.googleapis.com/maps/api/directions/json?alternatives=true&key=\(apiKey!)&destination=\(destination!)&origin=\(origin!)&mode=transit&transit_routing_preference=fewer_transfers&transit_mode=bus&language=tr")
+            .validate()
+            .responseDecodable(of: RouteMaps.self) { response in
             completion(response)
         }
     }
     
     // MARK: -Fetch PlaceID
     public func fetchPlaceID(completion: @escaping (_ result: DataResponse<PlaceIDRequest, AFError>) -> Void) {
-        AF.request("https://maps.googleapis.com/maps/api/geocode/json?key=\(apiKey!)&latlng=\(latitude!),\(longitude!)&language=tr&region=tr").responseDecodable(of: PlaceIDRequest.self) { response in
+        session.request("https://maps.googleapis.com/maps/api/geocode/json?key=\(apiKey!)&latlng=\(latitude!),\(longitude!)&language=tr&region=tr")
+            .validate()
+            .responseDecodable(of: PlaceIDRequest.self) { response in
             completion(response)
         }
     }
